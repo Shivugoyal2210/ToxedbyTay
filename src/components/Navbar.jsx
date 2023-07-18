0c62051e6f740dffb7cc5481989d8911d1659ef5
Shivu2210 <shivugoyal19@gmail.com>
2023-07-18 13:42:18 +0530

    added mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 import { mainLanding, logo } from '../assets/images';
 import { Link, NavLink } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import { useState } from 'react';
+
 const Navbar = () => {
   const activeStyles = {
     textDecoration: 'underline',
   };
   const navigate = useNavigate();
+
+  const [isMobileNav, setisMobileNav] = useState(false);
+
+  const handleNav = () => {
+    if (isMobileNav) {
+      setisMobileNav(false);
+    } else {
+      setisMobileNav(true);
+    }
+  };
 
   return (
     <div className="w-screen h-20 bg-opacity-0 z-10 absolute">
@@ -17,6 +31,13 @@ const Navbar = () => {
             className="logo_navbar h-12 object-contain cursor-pointer"
             src={logo}
           />
+        </div>
+        <div className="block lg:hidden mr-5 z-50" onClick={handleNav}>
+          {isMobileNav ? (
+            <MenuIcon style={{ color: '#545454' }} />
+          ) : (
+            <CloseIcon style={{ color: '#545454' }} />
+          )}
         </div>
         <div className="hidden lg:flex space-x-20 text-[19px] mr-10">
           <ul className="flex gap-10">
@@ -50,6 +71,47 @@ const Navbar = () => {
           </ul>
         </div>
       </nav>
+      <ul
+        className={
+          isMobileNav
+            ? 'mobile-nav lg:hidden flex flex-col gap-10 ease-out duration-200'
+            : 'fixed left-[100%]'
+        }
+      >
+        <li>
+          <NavLink
+            className="cursor-pointer"
+            to="/services"
+            onClick={() => {
+              setisMobileNav(false);
+            }}
+          >
+            Services
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="cursor-pointer"
+            to="/contact"
+            onClick={() => {
+              setisMobileNav(false);
+            }}
+          >
+            Contact
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="cursor-pointer"
+            to="/"
+            onClick={() => {
+              setisMobileNav(false);
+            }}
+          >
+            Book Now
+          </NavLink>
+        </li>
+      </ul>
     </div>
   );
 };
