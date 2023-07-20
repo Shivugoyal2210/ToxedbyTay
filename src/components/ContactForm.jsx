7c703d216c32f4e0954b68ecca9e3ae02f3cf9d5
Shivu2210 <shivugoyal19@gmail.com>
2023-07-20 18:41:11 +0530

    mostly finished contact page

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,15 +1,177 @@
 import React from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { Formik } from 'formik';
+import {
+  Box,
+  Button,
+  TextField,
+  useMediaQuery,
+  Typography,
+  useTheme,
+} from '@mui/material';
+import * as yup from 'yup';
+import Faq from './Faq';
 
 const ContactForm = () => {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyCKt-9unVURf4GzKYwukndCxAGD2w2aIvE',
+  const big = useMediaQuery('(min-width:960px)');
+  const messageSchema = yup.object().shape({
+    firstName: yup.string().required('required'),
+    lastName: yup.string(),
+    email: yup.string().email('invalid email').required('required'),
+    message: yup.string().required('required'),
   });
+
+  const initialValuesMessage = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: '',
+  };
+
+  const handleFormSubmit = () => {
+    return;
+  };
   return (
-    <GoogleMap
-      zoom={10}
-      center={{ lat: 33.627, lng: -112.06618194683227 }}
-    ></GoogleMap>
+    <>
+      <div>
+        <div className="w-screen px-10 pt-[20rem] lg:flex justify-between">
+          <div className="lg:w-[40%]">
+            <a
+              className="underline"
+              href="https://www.google.com/maps/place/15440+N+7th+St+%2318,+Phoenix,+AZ+85022/@33.6271599,-112.0685713,17z/data=!3m1!4b1!4m5!3m4!1s0x872b6e1210ec26ef:0xb1270d207c582acf!8m2!3d33.6271599!4d-112.0663826"
+            >
+              15440 N 7th Street, Suite 18, Phoenix, AZ 85022
+            </a>
+            <br />
+            <a className="underline" href="tel:+14806489401">
+              (480)-648-9401
+            </a>
+            <p className="mt-8">
+              <span className="font-bold">Questions?</span> Send us a message!
+            </p>
+            <Formik
+              onSubmit={handleFormSubmit}
+              initialValues={initialValuesMessage}
+              validationSchema={messageSchema}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleBlur,
+                handleChange,
+                handleSubmit,
+                setFieldValue,
+                resetForm,
+              }) => (
+                <form
+                  onSubmit={handleSubmit}
+                  className="w-full flex flex-col gap-10 mb-10"
+                >
+                  <div className="flex flex-row gap-5 w-[90%]">
+                    <TextField
+                      label="First Name"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.firstName}
+                      name="firstName"
+                      error={
+                        Boolean(touched.firstName) && Boolean(errors.firstName)
+                      }
+                      helperText={touched.firstName && errors.firstName}
+                      className="w-[48%]"
+                    />
+                    <TextField
+                      label="Last Name"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.lastName}
+                      name="lastName"
+                      error={
+                        Boolean(touched.lastName) && Boolean(errors.lastName)
+                      }
+                      helperText={touched.lastName && errors.lastName}
+                      className="w-[48%]"
+                    />
+                  </div>
+                  <TextField
+                    label="Email"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.email}
+                    name="email"
+                    error={Boolean(touched.email) && Boolean(errors.email)}
+                    helperText={touched.email && errors.email}
+                  />
+                  <TextField
+                    label="Message"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.message}
+                    name="message"
+                    error={Boolean(touched.message) && Boolean(errors.message)}
+                    helperText={touched.message && errors.message}
+                    inputProps={{
+                      style: {
+                        height: '5rem',
+                      },
+                    }}
+                  />
+                  <input
+                    type="submit"
+                    value="SEND"
+                    className="bg-black text-white py-7 px-5 cursor-pointer w-1/2 "
+                  />
+                </form>
+              )}
+            </Formik>
+          </div>
+          <div className="lg:w-[45%]">Map to display</div>
+        </div>
+        <div className="about-me my-[5rem]">
+          <div className={big ? 'w-[40%] ml-[52%]' : 'w-[80%] mx-auto'}>
+            <p className="cursive text-8xl mb-10">hi, I'm Taylor…</p>
+            <p className="mb-5">
+              I am a Registered Nurse and Licensed Aesthetician.{' '}
+            </p>
+            <p className="mb-5">
+              I started my nursing career in the Intensive Care Unit (ICU). I
+              left the hospital in 2019 and found an amazing job working as a
+              Case Manager for an infusion pharmacy. I loved the job but missed
+              patient interaction.
+            </p>
+            <p className="mb-5">
+              In January 2021 I started learning Neuromodulators/Filler and love
+              it. I have set up a studio and am currently accepting new
+              patients. I love helping people feel beautiful and confident!
+            </p>
+            <p className="mb-5">
+              When I’m not working, I spend my time with my two Boxers, Benjamin
+              and Birdie. I love home decor, travel, and spending time with
+              friends and family.
+            </p>
+            <p className="font-bold">Certifications:</p>
+            <p>Registered Nurse, August 2015</p>
+            <p>Licensed Aesthetician, March 2010</p>
+            <p>
+              Aesthetic Medical Educators Training for Botulinum Toxin/ Dermal
+              Filler, January 2021
+            </p>
+            <p className="mb-5">Microneedling, November 2021</p>
+            <p className="font-bold">
+              Medical Director/Primary Injector Trainor
+            </p>
+            <p>Erin Jones, FNP/ PMHNP</p>
+            <input
+              type="button"
+              value="Book Now"
+              className="bg-black text-white mt-[8rem] py-6 px-5 w-1/2 cursor-pointer "
+            />
+          </div>
+        </div>
+        <Faq />
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Faq.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import serviceJson from '../assets/images/Services/serviceData.json';
+import { useState } from 'react';
+const Faq = () => {
+  const faq1 = serviceJson['Tox & Neuromodulators'].FAQs;
+  const faq2 = serviceJson['Fillers'].FAQs;
+  const faq3 = serviceJson['Skin Tightening'].FAQs;
+  faq1.forEach((item) => (item.open = false));
+  faq2.forEach((item) => (item.open = false));
+  faq3.forEach((item) => (item.open = false));
+  const [faq_1, setfaq_1] = useState(faq1);
+  const [faq_2, setfaq_2] = useState(faq2);
+  const [faq_3, setfaq_3] = useState(faq3);
+
+  const handleFaq1 = (index) => {
+    setfaq_1((prev) =>
+      prev.map((item) =>
+        prev.indexOf(item) != index ? item : { ...item, open: !item.open }
+      )
+    );
+  };
+
+  const handleFaq2 = (index) => {
+    setfaq_2((prev) =>
+      prev.map((item) =>
+        prev.indexOf(item) != index ? item : { ...item, open: !item.open }
+      )
+    );
+  };
+
+  const handleFaq3 = (index) => {
+    setfaq_3((prev) =>
+      prev.map((item) =>
+        prev.indexOf(item) != index ? item : { ...item, open: !item.open }
+      )
+    );
+  };
+  return (
+    <div className="my-10">
+      <p className="cursive text-7xl text-center mb-10 ">
+        Frequently Asked Questions
+      </p>
+      <h1 className="tracking-widest mb-7 w-[80%] mx-auto">NEUROMODULATORS</h1>
+      <div className="accordion w-[80%] mx-auto text-lg">
+        {faq_1.map((item, i) => (
+          <div
+            className="item cursor-pointer"
+            key={i}
+            onClick={() => handleFaq1(i)}
+          >
+            <div className="title">
+              <h2>
+                {item.question} <span>+</span>
+              </h2>
+            </div>
+            <div
+              className={
+                item.open ? 'fade-in-text content block mt-5' : 'hidden'
+              }
+            >
+              {item.answer}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <h1 className="tracking-widest mb-7 w-[80%] mx-auto mt-[5rem]">
+        FILLERS
+      </h1>
+      <div className="accordion w-[80%] mx-auto text-lg">
+        {faq_2.map((item, i) => (
+          <div
+            className="item cursor-pointer"
+            key={i}
+            onClick={() => handleFaq2(i)}
+          >
+            <div className="title">
+              <h2>
+                {item.question} <span>+</span>
+              </h2>
+            </div>
+            <div className={item.open ? 'fade-in-text block mt-5' : 'hidden'}>
+              {item.answer}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <h1 className="tracking-widest mb-7 w-[80%] mx-auto mt-[5rem]">
+        SKIN TIGHTENING
+      </h1>
+      <div className="accordion w-[80%] mx-auto text-lg">
+        {faq_3.map((item, i) => (
+          <div
+            className="item cursor-pointer"
+            key={i}
+            onClick={() => handleFaq3(i)}
+          >
+            <div className="title">
+              <h2>
+                {item.question} <span>+</span>
+              </h2>
+            </div>
+            <div className={item.open ? 'fade-in-text block mt-5' : 'hidden'}>
+              {item.answer}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Faq;
